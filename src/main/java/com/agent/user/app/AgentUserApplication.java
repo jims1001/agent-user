ebc038e6c82bf041983cf813cbae1153cd9867f7
jed <166913797+jims1001@users.noreply.github.com>
2025-05-31 13:57:15 +0400

    init project agent-user user authroize user check user roles

---
+++ src/main/java/com/agent/user/app/AgentUserApplication.java
@@ -0,0 +1,13 @@
+package com.agent.user.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agent/user/app/AgentUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agent.user.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
